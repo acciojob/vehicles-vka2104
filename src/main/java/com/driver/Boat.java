f91850b0487173a02fd8e67d15622768554c630f
anjana <anjana123@gmail.com>
2023-09-02 14:07:15 +0530

    created vehicle appliation

---
+++ src/main/java/com/driver/Boat.java
@@ -0,0 +1,34 @@
+package com.driver;
+
+public class Boat implements WaterVehicle{
+    private String name;
+    private int capacity;
+
+    public Boat(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public String getVehicleName() {
+        return this.getName();
+    }
+    public int getVehicleCapacity() {
+        return this.getCapacity();
+    }
+}
--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -3,11 +3,12 @@ package com.driver;
 public class F1 extends Car {
 
     public F1(String name, boolean isManual) {
+        super(name,4,2,6, isManual, "Race", 2);
         //Use arbitrary values for parameters which are not mentioned
     }
 
     public void accelerate(int rate){
-        int newSpeed = 0; //set the value of new speed by using currentSpeed and rate
+        int newSpeed = super.getCurrentSpeed()+rate; //set the value of new speed by using currentSpeed and rate
         /**
          * speed 0: gear 1
          * speed 1-50: gear 1
@@ -19,7 +20,21 @@ public class F1 extends Car {
          */
 
         if(newSpeed == 0) {
+            super.stop();
+            super.changeGear(1);
             //Stop the car, set gear as 1
+        } else if(newSpeed > 0 && newSpeed <= 50) {
+            super.changeGear(2);
+        } else if(newSpeed > 51 && newSpeed <= 100) {
+            super.changeGear(3);
+        } else if(newSpeed > 101 && newSpeed <= 150) {
+            super.changeGear(3);
+        } else if(newSpeed > 151 && newSpeed <= 200) {
+            super.changeGear(4);
+        } else if(newSpeed > 201 && newSpeed <= 250) {
+            super.changeGear(5);
+        } else if(newSpeed > 250 ) {
+            super.changeGear(6);
         }
         //for all other cases, change the gear accordingly
 
