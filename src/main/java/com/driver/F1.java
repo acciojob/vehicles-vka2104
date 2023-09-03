8b85980f80cc32a2c6b7efb2a536aa858c94d4fc
anjana <anjana123@gmail.com>
2023-09-03 12:23:54 +0530

    fixed the review changes

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -23,17 +23,17 @@ public class F1 extends Car {
             super.stop();
             super.changeGear(1);
             //Stop the car, set gear as 1
-        } else if(newSpeed > 0 && newSpeed <= 50) {
+        } else if(newSpeed <= 50) {
             super.changeGear(1);
-        } else if(newSpeed > 51 && newSpeed <= 100) {
+        } else if(newSpeed <= 100) {
             super.changeGear(2);
-        } else if(newSpeed > 101 && newSpeed <= 150) {
+        } else if(newSpeed <= 150) {
             super.changeGear(3);
-        } else if(newSpeed > 151 && newSpeed <= 200) {
+        } else if(newSpeed <= 200) {
             super.changeGear(4);
-        } else if(newSpeed > 201 && newSpeed <= 250) {
+        } else if(newSpeed <= 250) {
             super.changeGear(5);
-        } else if(newSpeed > 250 ) {
+        } else {
             super.changeGear(6);
         }
         //for all other cases, change the gear accordingly
